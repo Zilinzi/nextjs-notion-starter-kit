d23cd87b31f953c84fded6e24440d5969db8d8fa
Zilinzi <51425203+Zilinzi@users.noreply.github.com>
2023-08-21 05:57:07 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'e6c7a034e219421f81e6286e3508cedd',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Ai Blog',
+  domain: 'https://zilinvip.notion.site/zilinvip/e6c7a034e219421f81e6286e3508cedd',
+  author: 'zilin',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
